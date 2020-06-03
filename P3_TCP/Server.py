f4dcf5f91a2fc3bc0d63c5cf76788a32ffff6542
Max Halbert <maxhalbert4@csumb.edu>
2020-06-02 18:25:23 -0700

    Update Server.py

--- P3_TCP/Server.py
+++ P3_TCP/Server.py
@@ -28,3 +28,61 @@ while True:
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence.encode())
     connectionSocket.close()
+
+   from socket import *
+from threading import Thread 
+from SocketServer import ThreadingMixIn 
+
+# Multithreaded Python server : TCP Server Socket Thread Pool
+class ClientThread(Thread): 
+ 
+    def __init__(self,ip,port,conn,clientID): 
+        Thread.__init__(self) 
+        self.ip = ip 
+        self.port = port
+        self.conn = conn
+
+        if (clientID == 1):
+            print ("Accepted first connection, calling it client X")
+            clientName = 'X'
+            message = "Client X connected"
+        else:
+            print("Accepted second connection, calling it client Y")
+            clientName = 'Y'
+            message = "Client Y connected"
+        conn.send(message)
+ 
+    def run(self): 
+        while True:
+            data = self.conn.recv(1024)
+            if not data:
+                # client connection closed
+                break
+            else:
+                print "Server received data from ", clientName, data
+        self.conn.close()    # close the connection and this thread is done
+        
+            
+
+# Multithreaded Python server : TCP Server Socket Program Stub
+SERVER_IP = '10.0.0.2' # server hostname
+SERVER_PORT = 12000 
+BUFFER_SIZE = 1024  # Usually 1024, but we need quick response 
+
+serverSocket = socket(AF_INET, SOCK_STREAM)  
+# tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+serverSocket.bind((SERVER_IP, SERVER_PORT)) 
+threads = [] 
+
+serverSocket.listen(2)     
+print ("The server is ready to receive two connections...")
+clientID = 1
+while True: 
+    (connectionSocket, (ip,port)) = serverSocket.accept() 
+    newthread = ClientThread(ip,port,connectionSocket,clientID) 
+    newthread.start() 
+    threads.append(newthread)
+    clientID= clientID + 1
+ 
+for t in threads: 
+    t.join()  
