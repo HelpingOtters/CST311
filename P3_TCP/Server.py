8592f8dac9c94e334472333351a3fa1e285433ef
Dan Sedano <dandano2006@gmail.com>
2020-06-05 23:30:05 -0700

    broke larger methods into smaller ones

--- P3_TCP/Server.py
+++ P3_TCP/Server.py
@@ -2,88 +2,123 @@
 # Team: 3
 # Date: 06/09/2020
 # Title: Server.py
-# Description: This is a TCP Server program will will connect to two different
+# Description: This is a TCP Server program that will connect to two different
 # clients simultaneously. The server will receive one message from each server 
 # and will process each message. The server will then send the messages back to
 # the correct client.
 
 from socket import *
-from threading import Thread
+from threading import *
 from time import *
 
+# ****************** Team, lets keep these links here for future reference :) ******************
 # # This video talks about multithreaded servers https://www.youtube.com/watch?v=pPOBH21RnaA
 # # From Max: https://www.techbeamers.com/python-tutorial-write-multithreaded-python-server/
 
 
-# Multithreaded Python server : TCP Server Socket Thread Pool
-clientName = ""
-#xCtr = 0
-#yCtr = 0
+# Define global variables
+message_count = 0 # Count messages arriving at server
+first_client = "" # Store name of first client to connect
+
+# Define mutex variables 
+message_count_lock = Lock() # Lock for the message_count variable
+message_cv = Condition(message_count_lock) # A CV to wait for server to receive both messages 
 
 
-
-# sends the order awk to the clients    
-#def awk():
-
- #   if: ()
-  #      return awk1
-  #  else:
-   #     return awk2   
-
-
-
-class ClientThread(Thread): 
-
- 
-    # constructor 
-    def __init__(self,ip,port,conn,clientID): 
+# Creates a class for each client thread
+class ClientThread(Thread):  
+    # Constructor 
+    def __init__(self, ip, port, connection, client_id): 
         Thread.__init__(self) 
         self.ip = ip 
         self.port = port
         self.connection = connection
         self.client_name = ""
         self.ack_message = ""
-        self.timestamp = 0.0 # I think this is ok to delete ***********************
         # Assigns client names to thread and sets ack_message message
         if (client_id == 1):
             print ("Accepted first connection, calling it client X")
-            self.clientName = 'X'
-            self.greeting = "Client X connected"
+            self.client_name = "X"
+            self.ack_message = "Client X connected"
         else:
             print("Accepted second connection, calling it client Y")
-            self.clientName = 'Y'
-            self.greeting = "Client Y connected"
+            self.client_name = "Y"
+            self.ack_message = "Client Y connected"
     
-    def get_greeting(self):
-        return self.greeting
-    
+    def set_message_counter(self):
+        global message_cv
+        global message_count
+        message_cv.acquire()  # acquire the lock to update message counter
+                
+        # If this is the first message
+        if (message_count == 0):
+            message_count = message_count + 1 
+        else:
+            message_count = message_count + 1
+        message_cv.notify()   # to notify the main thread a message is received
+        message_cv.release()  # release the lock
+
+    # Returns the acknowledgment message
+    def get_ack_message(self):
+        return self.ack_message
+    # Returns the client's name
     def get_client_name(self):
         return self.client_name
     # Sends clients an acknowledgment message, allowing client to send message back
     def send_ack_message(self):
         self.connection.send(self.get_ack_message().encode())
-    def get_server_output(self):
-        return self.server_output
+    # Returns broadcast message
+    def get_server_broadcast(self):
+        #self.create_server_output()
+        self.server_broadcast = f"Client {self.client_name}: {self.client_message}"
+        return self.server_broadcast
     #Sends a message to client
     def send_message(self,message):
         self.connection.send(message.encode())
 
-# Broadcasts the order awk message to all clients
-def send_order_awk(threads,first_client):
-        # message is ready to send to all the clients
+    def received_from_client(self):
+        print("Client ", self.client_name, " sent message ", message_count, ": ", self.client_message)
+
+    def set_first_client(self):
+        global first_client
+        # store client_id of the client that returned the first message
+        if message_count == 1:
+            first_client = self.client_name
+
+
+    def run(self):
+        # self.server_output = ""
+        self.client_message = ""
+        while True:
+            self.client_message = self.connection.recv(1024).decode()
+            if not self.client_message:
+                # client connection closed
+                break
+            else:
+                self.set_message_counter()
+                self.set_first_client()
+                self.received_from_client()
+        self.connection.close()    # close the connection and this thread is done
+
+
+# --------------------------------------- End of Class --------------------------------------- #
+
+# Broadcasts the order of message received to all clients
+def create_broadcast_msg(threads,first_client):
     if first_client == "X":
         #constructs broadcast message
-        broadcast_message = f"{threads[0].get_server_output()} before {threads[1].get_server_output()}"
-
-        # sends broadcast message to all clients
-        for t in threads:
-            t.send_message(broadcast_message)
+        broadcast_message = f"{threads[0].get_server_broadcast()} before {threads[1].get_server_broadcast()}"
     else:
-        broadcast_message = f"{threads[1].get_server_output()} before {threads[0].get_server_output()}"
+        broadcast_message = f"{threads[1].get_server_broadcast()} before {threads[0].get_server_broadcast()}"
+    return broadcast_message
 
-        for t in threads:
-            t.send_message(broadcast_message)
+# broadcasts ack to clients
+def ack_broadcast_order(threads, broadcast_message):
+    for t in threads:
+        t.send_message(broadcast_message)
 
+
+# Main method
 def main():
     # Multithreaded Python server : TCP Server Socket Program Stub
     SERVER_PORT = 12000 
@@ -115,23 +150,10 @@ def main():
     message_cv.acquire()
     while (message_count != 2):
         message_cv.wait() # wait for a thread's notification for any message received
-
-    send_order_awk(threads,first_client)
-
-    ## TESTED METHOD OK TO DELETE THIS block
-    # # message is ready to send to all the clients
-    # if first_client == "X":
-    #     #constructs broadcast message
-    #     broadcast_message = f"{threads[0].get_server_output()} before {threads[1].get_server_output()}"
-
-    #     # sends broadcast message to all clients
-    #     for t in threads:
-    #         t.send_message(broadcast_message)
-    # else:
-    #     broadcast_message = f"{threads[1].get_server_output()} before {threads[0].get_server_output()}"
-
-    #     for t in threads:
-    #         t.send_message(broadcast_message)
+   
+    # broadcasts an awk to all clients
+    broadcast_message = create_broadcast_msg(threads,first_client)
+    ack_broadcast_order(threads,broadcast_message)
 
     print("Waiting a bit for clients to close connections")
 
@@ -140,6 +162,6 @@ def main():
 
     print("Done.")
 
-# Runs Main Method -------------------------------------------------------------
+# ------------------------------- Run Main ---------------------------------- #
 if __name__ == "__main__":
     main()
