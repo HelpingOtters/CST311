c2c4d5c1417d9f207cf3f57e3ea8a912074f55dd
Max Halbert <maxhalbert4@csumb.edu>
2020-06-07 12:39:29 -0700

    Combine set_first_client into set_message_counter

--- P3_TCP/Server.py
+++ P3_TCP/Server.py
@@ -2,7 +2,7 @@
 # Team: 3
 # Date: 06/09/2020
 # Title: Server.py
-# Description: This is a TCP Server program will will connect to two different
+# Description: This is a TCP Server program that will connect to two different
 # clients simultaneously. The server will receive one message from each server 
 # and will process each message. The server will then send the messages back to
 # the correct client.
@@ -11,145 +11,165 @@ from socket import *
 from threading import *
 from time import *
 
-# ****************** Team lets keep these links here for future reference :) ******************
+# ****************** Team, lets keep these links here for future reference :) ******************
 # # This video talks about multithreaded servers https://www.youtube.com/watch?v=pPOBH21RnaA
 # # From Max: https://www.techbeamers.com/python-tutorial-write-multithreaded-python-server/
 
 
-# Counter for counting messages arrived to server
-msgCtr = 0
-# Message to be sent to all clients
-msg = ""
-# A lock to protect the msgCtr update
-msgCtr_lock = Lock()
-# A condition variable to wait for message received by both client threads
-cvToSend = Condition(msgCtr_lock)
-# Stores the client name of the client that connected first
-first_client = ""
+# Define global variables
+message_count = 0 # Count messages arriving at server
+first_client = "" # Store name of first client to connect
+
+# Define mutex variables 
+message_count_lock = Lock() # Lock for the message_count variable
+message_cv = Condition(message_count_lock) # A CV to wait for server to receive both messages 
+
 
 # Creates a class for each client thread
-class ClientThread(Thread):  
-    # constructor 
-    def __init__(self,ip,port,conn,clientID): 
+class ClientConnection(Thread):  
+    # Constructor 
+    def __init__(self, ip, port, connection, client_id): 
         Thread.__init__(self) 
         self.ip = ip 
         self.port = port
-        self.conn = conn
-        self.clientName = ""
-        self.handshake = ""
-        self.timestamp = 0.0
-        # Assigns client names to thread and sets handshake message
-        if (clientID == 1):
+        self.connection = connection
+        self.client_name = ""
+        self.ack_message = ""
+        # Assigns client names to thread and sets ack_message message
+        if (client_id == 1):
             print ("Accepted first connection, calling it client X")
-            self.clientName = "X"
-            self.handshake = "Client X connected"
+            self.client_name = "X"
+            self.ack_message = "Client X connected"
         else:
             print("Accepted second connection, calling it client Y")
-            self.clientName = "Y"
-            self.handshake = "Client Y connected"
+            self.client_name = "Y"
+            self.ack_message = "Client Y connected"
+    
+    # sets the message rcvd counter and check if it is the first message received by server
+    def set_message_counter(self):
+        global message_cv
+        global message_count
+        global first_client
+        message_cv.acquire()  # acquire the lock to update message counter
+        
+        message_count = message_count + 1
+        
+        # If this is the first message received, set the client be the first
+        if (message_count == 1):      
+            first_client = self.client_name
+        
+        message_cv.notify()   # to notify the main thread a message is received
+        message_cv.release()  # release the lock
+
+    # Returns the acknowledgment message
+    def get_ack_message(self):
+        return self.ack_message
+    # Returns the client's name
+    def get_client_name(self):
+        return self.client_name
+    # Sends clients an acknowledgment message, allowing client to send message back
+    def send_ack_message(self):
+        self.connection.send(self.get_ack_message().encode())
+    # Returns broadcast message
+    def get_server_broadcast(self):
+        #self.server_broadcast = f"Client {self.client_name}: {self.client_message}"
+        self.server_broadcast = "Client " + self.client_name + ": " + self.client_message 
+        return self.server_broadcast
+    #Sends a message to client
+    def send_message(self,message):
+        self.connection.send(message.encode())
+
+    def received_from_client(self):
+        print("Client ", self.client_name, " sent message ", message_count, ": ", self.client_message)
+
+# Moved this code to set_message_counter() because we need to lock the message_count
+#    and first_client global variables as well when we check or change the values
+#    def set_first_client(self):
+#        global first_client
+#        # store client_id of the client that returned the first message
+#        if message_count == 1:
+#            first_client = self.client_name
+
 
     def run(self):
-        global msgCtr 
-        global first_client
-        self.server_output = ""
         self.client_message = ""
+        global message_count
         while True:
-            self.client_message = self.conn.recv(1024).decode()
+            self.client_message = self.connection.recv(1024).decode()
             if not self.client_message:
                 # client connection closed
                 break
             else:
-                cvToSend.acquire()  # acquire the lock to update message counter
-                
-                 # it is the first message
-                if (msgCtr == 0):
+                self.set_message_counter()
+                #self.set_first_client() - it is done within set_message_counter()
+                self.received_from_client()
+        self.connection.close()    # close the connection and this thread is done
 
-                    self.server_output = f"Client {self.clientName}: {self.client_message}" 
-                    msgCtr = msgCtr + 1 
-                    # store clientID of the client that returned the first message
-                    first_client = self.clientName
-                
-                # it is the second message
-                else:
 
-                    self.server_output = f"Client {self.clientName}: {self.client_message}" 
-                    #self.server_output = self.server_output + self.clientName + ": " + self.client_message
+# --------------------------------------- End of Class --------------------------------------- #
 
-                    msgCtr = msgCtr + 1
-                print("Client ", self.clientName, " sent message ", msgCtr, ": ", self.client_message)
-                
-                cvToSend.notify()   # to notify the main thread a message is received
-                cvToSend.release()  # release the lock
-               
-        self.conn.close()    # close the connection and this thread is done
+# Broadcasts the order of message received to all clients
+def create_broadcast_msg(connections,first_client):
+    if first_client == "X":
+        #constructs broadcast message
+        broadcast_message = f"{connections[0].get_server_broadcast()} before {connections[1].get_server_broadcast()}"
+    else:
+        broadcast_message = f"{connections[1].get_server_broadcast()} before {connections[0].get_server_broadcast()}"
+    return broadcast_message
 
-    # *************** Note for Team 2, OK to delete this comment. You can probably use this for the EC? ******************
-    # If not, then delete this method.
-    # Returns the thread's connetion socket
-    def get_connections(self):
-        return self.conn
-    # Returns the handshake message
-    def get_handshake(self):
-        return self.handshake
-    # Returns the client's name
-    def get_client_name(self):
-        return self.clientName
-    # Sends clients a handshake messaging allowing client to send message back
-    def send_handshake(self):
-        self.conn.send(self.get_handshake().encode())
-    def get_server_output(self):
-        return self.server_output
-    #Sends a message to client
-    def send_message(self,message):
-        self.conn.send(message.encode())
+# broadcasts ack to clients
+def send_broadcast_ack(connections, broadcast_message):
+    for c in connections:
+        c.send_message(broadcast_message)
 
-# Multithreaded Python server : TCP Server Socket Program Stub
-SERVER_PORT = 12000 
-BUFFER_SIZE = 1024  # Usually 1024, but we need quick response 
 
-serverSocket = socket(AF_INET, SOCK_STREAM)  
-serverSocket.bind(('', SERVER_PORT)) 
-threads = []
+# Main method
+def main():
+    # Multithreaded Python server : TCP Server Socket Program Stub
+    SERVER_PORT = 12000 
+    BUFFER_SIZE = 1024  # Usually 1024, but we need quick response 
 
-# using 2 as a parameter to allow for 2 connections to be queued
-serverSocket.listen(2)      
-print ("The server is ready to receive two connections...")
-clientID = 1 # used to identify the client
+    serverSocket = socket(AF_INET, SOCK_STREAM)  
+    serverSocket.bind(('', SERVER_PORT)) 
+    # Holds the connection threads
+    connections = []
 
-while True: 
-    (connectionSocket, (ip,port)) = serverSocket.accept() 
-    newthread = ClientThread(ip,port,connectionSocket,clientID) 
-    newthread.start() 
-    threads.append(newthread)
-    clientID = clientID + 1
+    # using 2 as a parameter to allow for 2 connections to be queued
+    serverSocket.listen(2)      
+    print ("The server is ready to receive two connections...\n")
+    client_id = 1 # used to identify the client
+
+    while True: 
+        (connectionSocket, (ip,port)) = serverSocket.accept() 
+        new_connection = ClientConnection(ip,port,connectionSocket,client_id) 
+        new_connection.start() 
+        connections.append(new_connection)
+        client_id = client_id + 1
+
+        # Checks that there is more than one client before finalizing the connection
+        if(len(connections) > 1):
+            for c in connections:
+                c.send_ack_message()
+            break
+    # wait for both connections to receive a message
+    print("\nWaiting to receive messages from client X and client Y....\n")
     
-    # Checks for even number of clients before sending handshake messages to clients.
-    if(len(threads) % 2 == 0):
-        for t in threads:
-            t.send_handshake()
-        break
-
-# wait for both threads to receive a message
-cvToSend.acquire()
-while (msgCtr != 2):
-    cvToSend.wait() # wait for a thread's notification for any message received
-
-# message is ready to send to all the clients
-if first_client == "X":
-    #constructs broadcast message
-    broadcast_message = f"{threads[0].get_server_output()} before {threads[1].get_server_output()}"
-
-    # sends broadcast message to all clients
-    for t in threads:
-        t.send_message(broadcast_message)
-else:
-    broadcast_message = f"{threads[1].get_server_output()} before {threads[0].get_server_output()}"
+    message_cv.acquire()
+    while (message_count != 2):
+        message_cv.wait() # wait for a thread's notification for any message received
+    message_cv.release()    
     
-    for t in threads:
-        t.send_message(broadcast_message)
+    # broadcasts an ack to all clients
+    broadcast_message = create_broadcast_msg(connections,first_client)
+    send_broadcast_ack(connections,broadcast_message)
 
-print("Waiting a bit for clients to close connections")
+    print("\nWaiting a bit for clients to close connections")
 
-for t in threads: 
-    t.join()
-print("Done.")
+    for c in connections: 
+        c.join()
+
+    print("Done.")
+
+# ------------------------------- Run Main ---------------------------------- #
+if __name__ == "__main__":
+    main()
