dc898d455af021de090a50f59bd57bb1708ccdc3
lindseyreynolds44 <56812063+lindseyreynolds44@users.noreply.github.com>
2020-06-08 17:42:29 -0700

    Completed EC

--- P3_TCP/EC_Client.py
+++ P3_TCP/EC_Client.py
@@ -7,71 +7,60 @@ Description: Extra Credit Assignment. This client will establish a TCP connectio
 to the server and wait until a second client connects with the same server. It will 
 then be able to send messages back and forth to the other client, through the server.
 
+NOTE: Must use Python 3.6.9 or greater
+
 """
 
 from socket import *
 from threading import *
-import time
 from os import *
 from signal import *
 
-# variables
+# Declare variables
 serverName = 'localhost'
 serverPort = 12000
 messageSize = 1024
-encoding ='utf-8'
 connectionOpen = False
 
-# function to receive messages through client socket
+# Function to continuously receive messages through client socket
 def receiveMessage():
-    global connectionOpen 
-    try:   
-        while True:
-            message = (clientSocket.recv(1024)).decode() 
-            if not message:
-                break
-            elif message.lower() == "bye":
-                connectionOpen = False
-                print(message, "\n", "Connection closed.", )
-                kill(getpid(),SIGINT)
-                break
+    global connectionOpen # Flag to indicate if the connection is open
+    # Recieve messages from the server
+    while True:
+        message = (clientSocket.recv(1024)).decode() 
+        if not message:
+            break
+        # If a client says bye, end the connection
+        elif message.lower() == "bye":
+            connectionOpen = False
             print(message)
-           
-    except error:
-        print("Bad Connection")
-        kill(getpid(),SIGINT) 
-   
+            print("Connection closed.")
+            kill(getpid(),SIGINT) # End the process
+            break
+        
+        print(message)
 
-# function to send messages via client socket
-def sendMessage(event=None):
-    try: # I added another try catch here to avoid the end of connection error - DS
-        clientSocket.send(str(message).encode('utf-8')) 
-    except error:
-        print("Bad Connection")
-        exit(1)
-
-# run main routine
+# Run main routine
 if __name__ == '__main__':
 
-    #setup client socket to server
+    # Setup client socket to server
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName,serverPort))
     connectionOpen = True
 
-    #create thread to receive messages via function
+    # Create thread to continuously receive messages
     receiveThread = Thread(target=receiveMessage)
     receiveThread.daemon = True
     receiveThread.start()
     
-    #loop keyboard input    
-    while True: 
+    # Loop while the client continues to send messages   
+    while connectionOpen: 
         try:
+            # Retrieve the clients message as input 
             message = input('')
-            sendMessage()
+            # Send client's message to the server
+            clientSocket.send(message.encode()) 
         except KeyboardInterrupt:
-            pass
-        
-        if not connectionOpen:
-            break
- 
-    clientSocket.close()
+            pass 
+    
+clientSocket.close()
--- P3_TCP/EC_Server.py
+++ P3_TCP/EC_Server.py
@@ -4,26 +4,25 @@ Team: 3
 Date: 06/09/2020
 Title: EC_Server.py
 Description: Extra Credit Assignment. This server will act as the processing 
-relay between to clients. That is, this server will receive two connections
-from two different clients. Each client will then send a message to the other 
-client via the server. Example Client X wants to send  message to Client Y. To
-do so it has to go through the server first (X -> S -> Y)
+relay between two clients. It will receive two connections from two different 
+clients. Each client will then send a message to the other client via the 
+server.
+
+NOTE: Must use Python 3.6.9 or greater
+
 """
 
 from socket import *
 from threading import *
-from time import *
 
 # ****************** Team, lets keep these links here for future reference :) ******************
 # # This video talks about multithreaded servers https://www.youtube.com/watch?v=pPOBH21RnaA
 # # From Max: https://www.techbeamers.com/python-tutorial-write-multithreaded-python-server/
 
-
-# Define global variables
-first_client = "" # Store name of first client to connect
+# Define global variable to hold both connections
 connections = []
 
-# Creates a class for each client thread
+# Creates a class for each client connection
 class ClientConnection(Thread):  
     # Constructor 
     def __init__(self, ip, port, connection, client_id): 
@@ -31,23 +30,18 @@ class ClientConnection(Thread):
         self.ip = ip 
         self.port = port
         self.connection = connection
-        self.client_name = ""
-        self.ack_message = ""
-        # Assigns client names to thread and sets ack_message message
+        self.ack_message = "\nBoth clients have connected. Begin chatting."
+        # Sets ack_message based on when this client connected
         if (client_id == 1):
-            print ("Accepted first connection, calling it client X")
-            self.client_name = "X"
-            self.ack_message = "Client X connected"
+            print ("Accepted first connection.")
         else:
-            print("Accepted second connection, calling it client Y")
-            self.client_name = "Y"
-            self.ack_message = "Client Y connected"
+            print("Accepted second connection.")
 
     # Returns the acknowledgment message
     def get_ack_message(self):
         return self.ack_message
 
-    # Sends clients an acknowledgment message, allowing client to send message back
+    # Sends the client an acknowledgment message, establishing the connection 
     def send_ack_message(self):
         try:     
             self.connection.send(self.get_ack_message().encode())
@@ -55,28 +49,35 @@ class ClientConnection(Thread):
             print("Bad Connection")
             exit(1)
 
-
-    #Sends a message to client
+    # Sends a message to the client
     def send_message(self,message):
         self.connection.send(message.encode())
 
+    # Method that runs when start() is called on a ClientConnection object
+    # This method recieves messages from one client and sends them to the 
+    # other client.
     def run(self):
         self.client_message = ""
+        # Continuously check for incoming messages 
         while True:
-            # So it doesn't error out when trying to send a connection to a client that is no longer connected
-            try:
-                self.client_message = self.connection.recv(1024).decode()
-            except error:
-                exit(0)
-                
+            self.client_message = self.connection.recv(1024).decode()
+
             if not self.client_message:
                 break
-    
-            for thread in connections:
-                thread.send_message(self.client_message)
+            
+            # If a client says "bye", close the connection
+            if self.client_message.lower() == "bye":
+                for thread in connections:
+                    thread.send_message(self.client_message)
+                break
                 
-
-        self.connection.close()    # close the connection and this thread is done
+            # Send the incoming message to the receiveing client, but not 
+            # the sending client
+            for thread in connections:
+                if thread.connection != self.connection:
+                    thread.send_message(self.client_message)
+                
+        self.connection.close() # end this connection 
 
 # --------------------------------------- End of Class --------------------------------------- #
 
@@ -91,32 +92,35 @@ def main():
     # Holds the connection threads
     global connections
 
-    # using 2 as a parameter to allow for 2 connections to be queued
+    # Use 2 as a parameter, to allow for 2 connections
     serverSocket.listen(2)      
     print ("The server is ready to receive two connections...\n")
-    client_id = 1 # used to identify the client
+    client_id = 1 # Used to identify the client
 
     # Accept arriving connections
     while True: 
         (connectionSocket, (ip,port)) = serverSocket.accept() 
+        # Create a new client connection
         new_connection = ClientConnection(ip,port,connectionSocket,client_id) 
         new_connection.daemon = True
         new_connection.start() 
+        # Add the new connection to the connections array
         connections.append(new_connection)
         client_id = client_id + 1
 
-        # Checks that there is more than one client before finalizing the connection
-        if(len(connections) > 1):
+        # Check that there are two clients before finalizing the connection
+        if(len(connections) == 2):
             for c in connections:
                 c.send_ack_message()
             break
 
     print("\nClients are chatting")
 
+    # Wait for both client connection threads to complete
     for c in connections: 
         c.join()
 
-    print("Done.")
+    print("Both connections closed.")
 
 # ------------------------------- Run Main ---------------------------------- #
 if __name__ == "__main__":
